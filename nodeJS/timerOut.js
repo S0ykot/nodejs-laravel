d74de4ea76fd3ec1b16b59f9e6bf1e5a00db0485
S0ykot <soykot.ch@gmail.com>
2020-02-05 03:30:01 +0600

    timeOut

---
+++ nodeJS/timerOut.js
@@ -0,0 +1,15 @@
+setTimeout(function() {
+	console.log("5s");
+},5000);
+
+setTimeout(function() {
+	console.log("3s")
+},3000);
+
+setTimeout(function() {
+	console.log("2s")
+},2000);
+
+setTimeout(function() {
+	console.log("1s")
+},1000);
